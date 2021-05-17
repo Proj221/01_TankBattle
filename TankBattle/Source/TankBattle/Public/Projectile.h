fddd592d9fef7461c1b494d30653a8d7a048df7d
Huawei <ravidren@126.com>
2021-05-17 21:44:28 +0800

    Ep193End_MortarFired!

--- TankBattle/Source/TankBattle/Public/Projectile.h
+++ TankBattle/Source/TankBattle/Public/Projectile.h
@@ -43,4 +43,9 @@ private:
 
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void OnTimerExpire();
+
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float DestroyDelay = 10.f;
 };
